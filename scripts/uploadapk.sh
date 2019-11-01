d7073cd742aeaa2d351e9356179782da3920c4c9
z020232 <lediya.nesakumari@rntbci.com>
2019-11-01 11:55:44 +0530

    apk.sh

---
+++ scripts/uploadapk.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+#1: LOG_USERNAME
+#2: LOG_PASSWORD
+#3: DRONE_BUILD_NUMBER
+#4: DRONE_PULL_REQUEST
+#5: GIT_USERNAME
+#6: GIT_TOKEN
+
+#DAV_URL=
+PUBLIC_URL=https://api.github.com/Lediya/SampleTest/app/
+USER=lediya.nesakumari@rntbci.com
+PASS=Lediya2019@@
+BUILD=1.0
+PR=$4
+GIT_USERNAME=Lediya
+GIT_TOKEN=23e387764c7d32821b002888fa84ddedb7c00fc2
+
+if ! test -e build/outputs/apk/qa/debug/qa-debug-*.apk ; then
+    exit 1
+fi
+echo "Uploaded artifact to $BUILD.apk"
+
+# delete all old comments, starting with "APK file:"
+oldComments=$(curl 2>/dev/null -u $GIT_USERNAME:$GIT_TOKEN -X GET https://api.github.com/Lediya/SampleTest/issues/$PR/comments | jq '.[] | (.id |tostring) + "|" + (.user.login | test("nextcloud-android-bot") | tostring) + "|" + (.body | test("APK file:.*") | tostring)'  | grep "true|true" | tr -d "\"" | cut -f1 -d"|")
+
+echo $oldComments | while read comment ; do
+    curl 2>/dev/null -u $GIT_USERNAME:$GIT_TOKEN -X DELETEhttps://api.github.com/Lediya/SampleTest/issues/comments/$comment
+done
+
+apt-get -y install testapp
+
+testapp -o $PR.png "$PUBLIC_URL/$BUILD.apk"
+
+#curl -u $USER:$PASS -X PUT $DAV_URL/$BUILD.apk --upload-file build/outputs/apk/qa/debug/qa-debug-*.apk
+#curl -u $USER:$PASS -X PUT $DAV_URL/$BUILD.png --upload-file $PR.png
+curl -u $USER:$PASS -X PUT $PUBLIC_URL/$BUILD.apk --upload-file build/outputs/apk/qa/debug/qa-debug-*.apk
+curl -u $USER:$PASS -X PUT $PUBLIC_URL/$BUILD.apk --upload-file build/outputs/apk/qa/debug/qa-debug-*.apk
+curl -X PUT -u $USER:$PASS -X PUT $BUILD.apk --upload-file build/outputs/apk/qa/debug/qa-debug-*.apk
+curl -u $GIT_USERNAME:$GIT_TOKEN -X POST  https://api.github.com/Lediya/TestApp/issues/comments/$PR/comments -d "{ \"body\" : \"APK file: $PUBLIC_URL/$BUILD.apk <br/><br/> ![qrcode]($PUBLIC_URL/$BUILD.png) <br/><br/>To test this change/fix you can simply download above APK file and install and test it in parallel to your existing Test app. \" }"
